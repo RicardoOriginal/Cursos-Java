35d8e0ed6cceebdcc6fd55a36081da9e9cfdfcdd
ricardo <ricardolima.ti@gmail.com>
2019-04-25 08:42:14 -0300

    Apredendo Scanner

---
+++ Java-io/src/br/com/ricardo/TesteLeitura2.java
@@ -0,0 +1,22 @@
+package br.com.ricardo;
+
+import java.io.*;
+
+public class TesteLeitura2 {
+
+    public static void main(String[] args) throws IOException {
+
+        InputStream fis = new FileInputStream(Constante.URL+"lorem2.txt");
+        Reader isr = new InputStreamReader(fis);
+        BufferedReader br = new BufferedReader(isr);
+
+        String linha = br.readLine();
+
+        while (linha != null) {
+            System.out.println(linha);
+            linha = br.readLine();
+        }
+
+        br.close();
+    }
+}
